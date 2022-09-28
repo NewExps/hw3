628e345e37e9fe57a3a29a197b80aa783ce6214a
NewExps <113742717+NewExps@users.noreply.github.com>
2022-09-28 16:09:46 -0500

    Update settings.py

--- demo/settings.py
+++ demo/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h&4kior16xzvax#s)kcz6ssd4#g=9f_-l6b9q-dt#18weqbkdu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['44.208.34.246']
+STATIC_ROOT = '/var/www/demo_poc/assets/'
 
 # Application definition
 
